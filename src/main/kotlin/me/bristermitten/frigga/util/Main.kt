e913cc0c9a0433bd2e7d7ab00eb106b636334fa2
Alex Wood <alexljwood24@hotmail.co.uk>
2020-06-13 23:36:13 +0100

    Improve a few examples

---
+++ src/main/kotlin/me/bristermitten/frigga/util/Main.kt
@@ -0,0 +1,44 @@
+package me.bristermitten.frigga.util
+
+import FriggaLexer
+import FriggaParser
+import me.bristermitten.frigga.loader.toAST
+import me.bristermitten.frigga.runtime.FriggaContext
+import me.bristermitten.frigga.runtime.entity.type.registerBuiltInTypes
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import kotlin.system.measureTimeMillis
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+@ExperimentalTime
+fun main() {
+    repeat(10) {
+        val code = ClassLoader.getSystemClassLoader().getResourceAsStream("samples/simple.fg")
+        val charStream = CharStreams.fromStream(code)
+
+        val (friggaLexer, lexTime) = measureTimedValue {
+            FriggaLexer(charStream)
+        }
+        println("Lexing took ${lexTime.inMilliseconds}ms")
+
+        val (file, parseTime) = measureTimedValue {
+            val parser = FriggaParser(CommonTokenStream(friggaLexer))
+            val parseTree = parser.friggaFile()
+            parseTree.toAST()
+        }
+        println("Parsed AST in ${parseTime.inMilliseconds}ms")
+
+
+        val execTime = measureTimeMillis {
+            val context = FriggaContext()
+            registerBuiltInTypes(context)
+            file.contents.forEach {
+                it.initialize(context)
+                it.eval(context.stack, context)
+            }
+        }
+
+        println("Executed program in $execTime ms")
+    }
+}
---
+++ src/test/kotlin/me/bristermitten/frigga/Tests.kt
@@ -0,0 +1,15 @@
+package me.bristermitten.frigga
+
+import org.junit.jupiter.api.Test
+import kotlin.time.ExperimentalTime
+
+class Tests {
+
+    @Test
+    @ExperimentalTime
+    fun test() {
+
+
+    }
+}
+
