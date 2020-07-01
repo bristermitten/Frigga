16302ec634b95d9ed52b71ec83248e5ba964179c
BuildTools <unconfigured@null.spigotmc.org>
2020-07-01 19:42:32 +0100

    Change the benchmark to `8!` and add some nicer messages

--- src/test/kotlin/me/bristermitten/frigga/SimpleBenchmark.kt
+++ src/test/kotlin/me/bristermitten/frigga/SimpleBenchmark.kt
@@ -17,10 +17,10 @@ class SimpleBenchmark : FriggaTest() {
                 if(value == 1, yield[1])
                 yield(value * factorial(value - 1))
             }    
-            factorial(5) 
+            factorial(8) 
         """.trimIndent()
 
-        val times = 100
+        val times = 1000
         var totalParseTime = 0.0.toBigDecimal()
         var totalExecutionTime = 0.0.toBigDecimal()
         var totalTime = 0.0.toBigDecimal()
@@ -31,13 +31,16 @@ class SimpleBenchmark : FriggaTest() {
             totalExecutionTime += result.timings.runtimeTime.toBigDecimal()
             totalTime += result.timings.totalTime.toBigDecimal()
 
-            result.leftoverStack.first() shouldBe intValue(120)
+            result.leftoverStack.first() shouldBe intValue(factorial(8))
         }
+
+        val averageFriggaExecutionTime = totalExecutionTime / times.toBigDecimal()
         println(
             """
+            ** Calculation of 8! **
             ==Frigga==
             Average Parse Time = ${totalParseTime / times.toBigDecimal()} ms
-            Average Execution Time = ${totalExecutionTime / times.toBigDecimal()} ms
+            Average Execution Time = $averageFriggaExecutionTime ms
             Average Total Time = ${totalTime / times.toBigDecimal()} ms
 
         """.trimIndent()
@@ -48,19 +51,19 @@ class SimpleBenchmark : FriggaTest() {
         totalTime = 0.0.toBigDecimal()
 
         repeat(times) {
-            val (_, time) = measureTimedValue { factorial(5) }
+            val (_, time) = measureTimedValue { factorial(8) }
 
             totalTime += time.inMilliseconds.toBigDecimal()
         }
-        println(
-            """
+        println("""
             ==Kotlin==
             Average Execution Time = ${totalTime / times.toBigDecimal()} ms
-        """.trimIndent()
-        )
+            
+            Therefore, JVM Compiled Kotlin is about ${averageFriggaExecutionTime / (totalTime / times.toBigDecimal())}x faster than interpreted Frigga.
+        """.trimIndent())
     }
 
-    private fun factorial(value: Int): Int {
+    private fun factorial(value: Int): Long {
         if (value == 1) {
             return 1
         }
